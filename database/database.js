14b9e6c3876c6ecb25b1686c6ad203f6dea67773
danilo <dxsuporteti@gmail.com>
2024-03-14 17:44:54 -0300

    rm mysql, i sqlite

---
+++ database/database.js
@@ -0,0 +1,28 @@
+const sqlite3 = require("sqlite3").verbose();
+const filepath = "./database/database.sql";
+
+const DataBase = new sqlite3.Database(filepath, (error) => {
+  if (error) {
+    return console.error(error.message);
+  }
+  //createTable(DataBase);
+});
+
+function createTable(DataBase) {
+  DataBase.exec(`
+    CREATE TABLE user
+    (
+      ID INTEGER PRIMARY KEY AUTOINCREMENT,
+      email   VARCHAR(255) NOT NULL,
+      password   VARCHAR(255) NOT NULL
+    );
+    CREATE TABLE product
+    (
+      ID INTEGER PRIMARY KEY AUTOINCREMENT,
+      name   VARCHAR(255) NOT NULL,
+      price   INT(10) NOT NULL
+    );
+  `);
+}
+
+module.exports = DataBase;
--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-const { app, BrowserWindow, ipcMain, Notification } = require('electron');
-const path = require('path');
-let db = require('./database')
+const { app, BrowserWindow, ipcMain, Notification } = require("electron");
+const Path = require("path");
+const DataBase = require("./database/database");
 
 let win;
 let winlogin;
@@ -12,11 +12,10 @@ function createWindow() {
       // nodeIntegration: true,
       // contextIsolation:true,
       // devTools:false,
-      preload: path.join(__dirname, 'index.js')
-
-    }
-  })
-  win.loadFile('index.html')
+      preload: Path.join(__dirname, "view/index/index.js"),
+    },
+  });
+  win.loadFile("view/index/index.html");
 }
 
 function loginWindow() {
@@ -27,136 +26,128 @@ function loginWindow() {
       // nodeIntegration: true,
       // contextIsolation:true,
       // devTools:false,
-      preload: path.join(__dirname, 'login.js')
-
-    }
-  })
-  winlogin.loadFile('login.html')
+      preload: Path.join(__dirname, "view/login/login.js"),
+    },
+  });
+  winlogin.loadFile("view/login/login.html");
 }
 
-app.whenReady().then(loginWindow)
+app.whenReady().then(loginWindow);
 
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit()
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
   }
-})
-
-app.on('activate', () => {
-  if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow()
-  }
-})
-
-ipcMain.handle('login', (event, obj) => {
-  validatelogin(obj)
 });
 
+app.on("activate", () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
+
+ipcMain.handle("login", (event, obj) => {
+  validatelogin(obj);
+});
 
 function validatelogin(obj) {
-  const { email, password } = obj
-  const sql = "SELECT * FROM user WHERE email=? AND password=?"
-  db.query(sql, [email, password], (error, results, fields) => {
-    if (error) { console.log(error); }
+  const { email, password } = obj;
+  const sql = "SELECT * FROM user WHERE email=? AND password=?";
+  DataBase.all(sql, [email, password], (error, results, fields) => {
+    if (error) {
+      console.log(error);
+    }
 
     if (results.length > 0) {
-      createWindow()
-      win.show()
-      winlogin.close()
+      createWindow();
+      win.show();
+      winlogin.close();
     } else {
       new Notification({
         title: "login",
-        body: 'email o password equivocado'
-      }).show()
+        body: "email o password equivocado",
+      }).show();
     }
-
   });
 }
 
-
-
-ipcMain.handle('get', () => {
-  getProducts()
+ipcMain.handle("get", () => {
+  getProducts();
 });
 
-
-ipcMain.handle('add', (event, obj) => {
-  addProduct(obj)
+ipcMain.handle("add", (event, obj) => {
+  addProduct(obj);
 });
 
-
-ipcMain.handle('get_one', (event, obj) => {
-  getproduct(obj)
+ipcMain.handle("get_one", (event, obj) => {
+  getproduct(obj);
 });
 
-
-ipcMain.handle('remove_product', (event, obj) => {
-  deleteproduct(obj)
+ipcMain.handle("remove_product", (event, obj) => {
+  deleteproduct(obj);
 });
 
-
-ipcMain.handle('update', (event, obj) => {
-  updateproduct(obj)
+ipcMain.handle("update", (event, obj) => {
+  updateproduct(obj);
 });
-
 
 function getProducts() {
-
-  db.query('SELECT * FROM product', (error, results, fields) => {
+  DataBase.all("SELECT * FROM product", (error, results, fields) => {
     if (error) {
       console.log(error);
     }
 
-    win.webContents.send('products', results)
+    win.webContents.send("products", results);
   });
 }
-
 
 function addProduct(obj) {
-  const sql = "INSERT INTO product SET ?";
-  db.query(sql, obj, (error, results, fields) => {
+  /* const sql = "INSERT INTO product SET ?";
+  DataBase.all(sql, obj, (error, results, fields) => {
     if (error) {
       console.log(error);
     }
-    getProducts()
+    getProducts();
+  }); */
+
+  DataBase.run("INSERT INTO product(name, price) VALUES(?, ?)", obj, (err) => {
+    if (err) {
+      return console.log(err.message);
+    }
+    getProducts();
   });
 }
-
 
 function deleteproduct(obj) {
-  const { id } = obj
-  const sql = "DELETE FROM product WHERE id = ?"
-  db.query(sql, id, (error, results, fields) => {
+  const { id } = obj;
+  const sql = "DELETE FROM product WHERE id = ?";
+  DataBase.all(sql, id, (error, results, fields) => {
     if (error) {
       console.log(error);
     }
-    getProducts()
+    getProducts();
   });
 }
-
 
 function getproduct(obj) {
-  let { id } = obj
-  let sql = "SELECT * FROM product WHERE id = ?"
-  db.query(sql, id, (error, results, fields) => {
+  let { id } = obj;
+  let sql = "SELECT * FROM product WHERE id = ?";
+  DataBase.all(sql, id, (error, results, fields) => {
     if (error) {
       console.log(error);
     }
-    console.log(results)
-    win.webContents.send('product', results[0])
+    console.log(results);
+    win.webContents.send("product", results[0]);
   });
 }
-
 
 function updateproduct(obj) {
-  let { id, name, price } = obj
+  let { id, name, price } = obj;
   const sql = "UPDATE product SET name=?, price=? WHERE id=?";
-  db.query(sql, [name, price, id], (error, results, fields) => {
+  DataBase.all(sql, [name, price, id], (error, results, fields) => {
     if (error) {
       console.log(error);
     }
-    getProducts()
+    getProducts();
   });
 }
-
-
