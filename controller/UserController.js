24b6d93ebea2b629e69b51a4650638038bb44bf9
danilo <dxsuporteti@gmail.com>
2024-04-07 16:37:09 -0300

    debug login

--- controller/UserController.js
+++ controller/UserController.js
@@ -15,10 +15,10 @@ module.exports = new (class RegisterController {
 
   async destroy() {}
 
-  async login(obj) {
+  async login(data) {
     try {
-      const password = createHmac('sha256', process.env.APP_KEY).update(obj.password).digest('hex')
-      const result = await DataBase(DataTable).where({ username: obj.username, password: password }).first()
+      const password = createHmac('sha256', process.env.APP_KEY).update(data.password).digest('hex')
+      const result = await DataBase(DataTable).where({ username: data.username, password: password }).first()
       process.env.authUser = result.username
       process.env.authActive = result.active
       return result
