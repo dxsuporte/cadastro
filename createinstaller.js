106819d9bb72edfc89921b037884ad5bbc8ae806
danilo <dxsuporteti@gmail.com>
2024-04-12 08:45:02 -0300

    up

--- createinstaller.js
+++ createinstaller.js
@@ -4,12 +4,11 @@ const start = async () => {
   try {
     await Winstaller.createWindowsInstaller({
       appDirectory: 'build/Cadastro-win32-x64',
-      outputDirectory: 'build/installer64',
-      authors: 'DX Suporte - Danilo Xavier',
-      noMsi: true,
+      outputDirectory: 'build/installer',
       exe: 'Cadastro.exe',
       setupExe: 'Cadastro.exe',
-      setupIcon: './public/img/favicon.ico',
+      setupIcon: 'public/img/favicon.ico',
+      iconUrl: 'public/img/favicon.ico',
     })
   } catch (error) {
     console.log(`No dice: ${error.message}`)
